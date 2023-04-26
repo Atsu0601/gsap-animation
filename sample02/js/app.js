d4325570ee91ce6ddbe473f0278deb2e1e251776
Kawatani Atsushi <katsushi61@gmail.com>
2023-04-26 09:14:01 +0900

    Add js description comment

--- sample02/js/app.js
+++ sample02/js/app.js
@@ -1,18 +1,26 @@
+// カスタムカーソルとフォロワー要素を取得
 var cursor = $(".cursor"),
 follower = $(".cursor-follower");
 
+// 座標の初期化
 var posX = 0,
-    posY = 0;
+posY = 0;
 
 var mouseX = 0,
-    mouseY = 0;
+mouseY = 0;
 
+// アニメーションの設定
+// TweenMaxの引数(target:アニメーションの対象, duration:アニメーション, vars:プロパティと値を格納したオブジェクト)
+// 下記の場合
+// TweenMaxのtargetは空。durationは0.016秒。varsは繰り返しアニメーションのオプジェクト
 TweenMax.to({}, 0.016, {
-  repeat: -1,
+  repeat: -1, // 無限に繰り返す
   onRepeat: function() {
+    // カーソルの位置とマウスの位置を徐々に近づける
     posX += (mouseX - posX) / 9;
     posY += (mouseY - posY) / 9;
     
+    // フォロワー要素の座標を更新
     TweenMax.set(follower, {
         css: {    
         left: posX - 12,
@@ -20,6 +28,7 @@ TweenMax.to({}, 0.016, {
         }
     });
     
+    // カスタムカーソル要素の座標を更新
     TweenMax.set(cursor, {
         css: {    
         left: mouseX,
@@ -29,16 +38,23 @@ TweenMax.to({}, 0.016, {
   }
 });
 
+// jQueryで処理
+// マウスムーブイベントの処理
 $(document).on("mousemove", function(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+  // マウスの位置を更新
+  mouseX = e.clientX;
+  mouseY = e.clientY;
 });
 
+// jQueryで処理
+// リンクのマウスオーバー/マウスアウトイベントの処理
 $(".link").on("mouseenter", function() {
-    cursor.addClass("active");
-    follower.addClass("active");
+  // カーソルとフォロワー要素に "active" クラスを追加
+  cursor.addClass("active");
+  follower.addClass("active");
 });
 $(".link").on("mouseleave", function() {
-    cursor.removeClass("active");
-    follower.removeClass("active");
+  // カーソルとフォロワー要素から "active" クラスを削除
+  cursor.removeClass("active");
+  follower.removeClass("active");
 });
