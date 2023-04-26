284e2b374e407536b2b1f830069e553377fbcd1c
Kawatani Atsushi <katsushi61@gmail.com>
2023-04-26 09:19:22 +0900

    Add js description comment

--- sample03/js/app.js
+++ sample03/js/app.js
@@ -1,14 +1,16 @@
-// envelope animation
+// ウィンドウが読み込まれたときにアニメーションを実行
 window.onload = function(){
+    // アニメーションのタイムラインを作成し、1秒のディレイを設定
     var tl = new TimelineLite({delay: 1}),
     firstBg = document.querySelectorAll('.text__first-bg'),
     secBg = document.querySelectorAll('.text__second-bg'),
     word  = document.querySelectorAll('.text__word');
     
+    // タイムラインにアニメーションを追加
     tl
-    .to(firstBg, 0.2, {scaleX:1})
-    .to(secBg, 0.2, {scaleX:1})
-    .to(word, 0.1, {opacity:1}, "-=0.1")  
-    .to(firstBg, 0.2, {scaleX:0})
-    .to(secBg, 0.2, {scaleX:0});
+    .to(firstBg, 0.2, {scaleX:1}) // 最初の背景要素の scaleX を 0 から 1 に変更
+    .to(secBg, 0.2, {scaleX:1}) // 2番目の背景要素の scaleX を 0 から 1 に変更
+    .to(word, 0.1, {opacity:1}, "-=0.1") // テキストの opacity を 0 から 1 に変更し、前のアニメーションから 0.1 秒差で開始
+    .to(firstBg, 0.2, {scaleX:0}) // 最初の背景要素の scaleX を 1 から 0 に変更
+    .to(secBg, 0.2, {scaleX:0}); // 2番目の背景要素の scaleX を 1 から 0 に変更
 }
